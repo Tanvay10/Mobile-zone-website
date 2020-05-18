9826e4d20a10742407128e370c7841f41496f552
tanvay vaidya <vaidyat78@gmail.com>
2020-05-18 16:08:22 +0530

    Second Push

---
+++ Electricks-shop/pages/supplier_delete.php
@@ -0,0 +1,12 @@
+
+    <!-- dynamic content will be here -->
+<?php
+
+include '../config/dbconn.php';
+
+$id = $_GET['supp_id'];
+
+$result = mysqli_query($dbconn, "DELETE FROM supplier WHERE supp_id=$id");
+
+header("Location:admin_panel.php");
+?>
